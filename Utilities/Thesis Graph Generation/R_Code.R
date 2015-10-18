b01866fefb6f7cabc2b7e27609a9fab43b62e8b2
lutrasdebtra <stuy.bradley@gmail.com>
2015-10-18 15:47:22 +1300

    saved r code

--- Utilities/Thesis Graph Generation/R_Code.R
+++ Utilities/Thesis Graph Generation/R_Code.R
@@ -36,14 +36,14 @@ par(mfrow=c(1, 1))
 
 trans.mat <- matrix(c(
   0,1,0,0,
-  0.5,0,0,0.5*0.5,
+  0.5,0,0.25,0.25,
   0,0,0,1,
-  0,0.5*0.5,0.5,0
+  0.25,0.25,0.5,0
 )
 , 4,4, byrow = TRUE)
 
 # GTR Whole Tree Borrowing Validation
-plot(density(rbinom(200000,20,0.4814815), adjust=10), col="green",lwd=2, 
+plot(density(rbinom(200000,20,0.57), adjust=10), col="green",lwd=2, 
      main="Simulation of 100,000 language evolutions under the GTR model", xlab="Number of cogantes", xlim=range(0:20))
 lines(density(gtrborrowtree$V1, adjust=10), col="blue", lwd=2)
 legend('topright',c("Binomial Distribution","Algorithm 1"), lty=c(1,1), lwd=c(2,2),col=c("green","blue"))
