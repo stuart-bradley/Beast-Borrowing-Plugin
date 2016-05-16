f00b38347bf054f3f23c18e78c4c90202817bb32
lutrasdebtra <stuy.bradley@gmail.com>
2016-05-16 17:49:35 +1200

    Analysis Objects are now created on a per rate basis, GC couldn't handle holding 100*9 objects each with a few 100MB of data.

--- src/beastborrowingplugin/thesisanalysis/BatchXMLAnalysis.java
+++ src/beastborrowingplugin/thesisanalysis/BatchXMLAnalysis.java
@@ -17,13 +17,15 @@ public class BatchXMLAnalysis {
 	protected HashMap<String, File> logs = new HashMap<String, File>();
 	protected HashMap<String, File> trees = new HashMap<String, File>();
 	protected HashMap<String, File> inputs = new HashMap<String, File>();
+	protected ArrayList<ArrayList<Double>> heightPercentageDifferences = new ArrayList<ArrayList<Double>>();
+	protected ArrayList<ArrayList<Double>> topologyDifferences = new ArrayList<ArrayList<Double>>();
 	static final int[] BORROWRATES = {0,1,5,10,15,20,30,40,50};
-	
-	protected HashMap<String, HashMap<String, AnalysisObject>> analysisObjects = new HashMap<String, HashMap<String, AnalysisObject>>();
+
+	protected HashMap<String, AnalysisObject> analysisObjects = new HashMap<String, AnalysisObject>();
 
 	public BatchXMLAnalysis(String logFileDir, String treeFileDir, String inputFileDir) {
 		String[] dirs = {logFileDir, treeFileDir, inputFileDir}; 
-		
+		System.out.println("Reading in files.");
 		for (String dir : dirs) {
 			File directory = new File(dir);
 			File[] files = directory.listFiles();
@@ -39,92 +41,90 @@ public class BatchXMLAnalysis {
 				}
 			}
 		}
-		
+
+		System.out.println("Creating Analysis Objects.");
 		for (int rate : BORROWRATES) {
-			analysisObjects.put(""+rate, new HashMap<String, AnalysisObject>());
+			analysisObjects = new HashMap<String,AnalysisObject>();
 			for (int i = 0; i < 100; i++) {
 				File log = logs.get("GTR_new_"+rate+"_"+i+".log");
 				File tree = trees.get("GTR_new_"+rate+"_"+i+".trees");
 				File input = inputs.get("GTR_Borrow_"+rate+"_"+i+"_Input.xml");
 				if (log != null && tree != null && input != null) {
-					analysisObjects.get(""+rate).put(""+i, new AnalysisObject(log, tree, input));
+					System.out.println("Create:" + rate +"_"+i);
+					analysisObjects.put(""+i, new AnalysisObject(log, tree, input));
 				}
 			}
+			analyseTopology("C:/Users/Stuart/Downloads/BorrowingComparisons/Results",rate);
+			analyseHeights("C:/Users/Stuart/Downloads/BorrowingComparisons/Results",rate);
 		}
-	}
-	
-	protected void analyseHeights(String loc) {
-		ArrayList<ArrayList<Double>> heightPercentageDifferences = new ArrayList<ArrayList<Double>>();
-		for (int rate : BORROWRATES) {
-			HashMap <String, AnalysisObject> rateObjects = analysisObjects.get(""+rate);
-			ArrayList<Double> rateHeights = new ArrayList<Double>();
-			rateHeights.add((double) rate);
-			//iterating over values only
-			for (AnalysisObject a : rateObjects.values()) {
-				Double totalNumber = 0.0;
-				Double totalDiff  = 0.0;
-				Double startingTreeHeight = a.startingTreeHeight;
-				for (Double treeHeight : a.heights) {
-					totalNumber++;
-					totalDiff += Math.abs(startingTreeHeight - treeHeight);
-					
-				}
-				rateHeights.add((totalDiff/totalNumber) /startingTreeHeight);
-			}
-			heightPercentageDifferences.add(rateHeights);
-		}
+
 		listToCSV(heightPercentageDifferences, "BorrowingComparisons/Results/heights.csv");
+		listToCSV(topologyDifferences, "BorrowingComparisons/Results/quartet.csv");
 	}
-	
-	protected void analyseTopology(String loc) {
-		ArrayList<ArrayList<Double>> topologyDifferences = new ArrayList<ArrayList<Double>>();
+
+	protected void analyseHeights(String loc, int rate) {
+		ArrayList<Double> rateHeights = new ArrayList<Double>();
+		rateHeights.add((double) rate);
+		//iterating over values only
+		for (AnalysisObject a : analysisObjects.values()) {
+			Double totalNumber = 0.0;
+			Double totalDiff  = 0.0;
+			Double startingTreeHeight = a.startingTreeHeight;
+			for (Double treeHeight : a.heights) {
+				totalNumber++;
+				totalDiff += Math.abs(startingTreeHeight - treeHeight);
+
+			}
+			rateHeights.add((totalDiff/totalNumber) /startingTreeHeight);
+		}
+		heightPercentageDifferences.add(rateHeights);
+	}
+
+	protected void analyseTopology(String loc, int rate) {
 		File resFolder = new File(loc+"/Tmp");
 		File qtDist = new File("qDist/quartet_dist.exe");
 		resFolder.mkdir();
-		for (int rate : BORROWRATES) {
-			System.out.println("Analysing rate: " + rate);
-			HashMap <String, AnalysisObject> rateObjects = analysisObjects.get(""+rate);
-			ArrayList<Double> rateTopologies = new ArrayList<Double>();
-			rateTopologies.add((double) rate);
-			
-			int num = 1;
-			int totalNum = rateObjects.keySet().size();
-			for (Map.Entry<String, AnalysisObject> entry : rateObjects.entrySet()) {
-				System.out.println("Analysing object: " + num +"/"+totalNum);
-				num++;
-				File startPath = new File(resFolder.getPath()+"/startTree_"+entry.getKey()+".tree");
-				createTreeFile(startPath.getPath(), entry.getValue().startingTree);
-				List<String> resTrees = entry.getValue().trees;
-				Double totalDiff = 0.0;
-				for (int i = 0; i < resTrees.size(); i++) {
-					try {
-						String t = resTrees.get(i);
-						File treePath = new File(resFolder.getPath()+"/tree_"+entry.getKey()+"_"+i+".tree");
-						createTreeFile(treePath.getPath(), t);
-						ProcessBuilder builder = new ProcessBuilder(
-					            "cmd.exe", "/c", qtDist.getAbsolutePath(), "-v", "\""+startPath.getAbsolutePath()+"\"", "\""+treePath.getAbsolutePath()+"\"");
-					    builder.redirectErrorStream(true);
-					    Process p = builder.start();
-					    BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					    String line;
-					    while ((line = r.readLine()) != null) {
-					    	totalDiff  += Double.parseDouble(line.split("\t")[3]);
-					    }
-					} catch (Exception e) {}
-				}
-				rateTopologies.add(totalDiff/resTrees.size());
+		System.out.println("Analysing rate: " + rate);
+		ArrayList<Double> rateTopologies = new ArrayList<Double>();
+		rateTopologies.add((double) rate);
+
+		int num = 1;
+		int totalNum = analysisObjects.keySet().size();
+		for (Map.Entry<String, AnalysisObject> entry : analysisObjects.entrySet()) {
+			System.out.println("Analysing object: " + num +"/"+totalNum);
+			num++;
+			File startPath = new File(resFolder.getPath()+"/startTree_"+entry.getKey()+".tree");
+			createTreeFile(startPath.getPath(), entry.getValue().startingTree);
+			List<String> resTrees = entry.getValue().trees;
+			Collections.shuffle(resTrees);
+			Double totalDiff = 0.0;
+			for (int i = 0; i < 300; i++) {
+				try {
+					String t = resTrees.get(i);
+					File treePath = new File(resFolder.getPath()+"/tree_"+entry.getKey()+"_"+i+".tree");
+					createTreeFile(treePath.getPath(), t);
+					ProcessBuilder builder = new ProcessBuilder(
+							"cmd.exe", "/c", qtDist.getAbsolutePath(), "-v", "\""+startPath.getAbsolutePath()+"\"", "\""+treePath.getAbsolutePath()+"\"");
+					builder.redirectErrorStream(true);
+					Process p = builder.start();
+					BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					String line;
+					while ((line = r.readLine()) != null) {
+						totalDiff  += Double.parseDouble(line.split("\t")[3]);
+					}
+				} catch (Exception e) {}
 			}
-			topologyDifferences.add(rateTopologies);
+			rateTopologies.add(totalDiff/resTrees.size());
 		}
-		
+		topologyDifferences.add(rateTopologies);
+
 		try {
 			delete(resFolder);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		listToCSV(topologyDifferences, "BorrowingComparisons/Results/quartet.csv");
 	}
-	
+
 	private void createTreeFile(String fileName, String tree) {
 		FileWriter fW = null;
 
@@ -142,14 +142,14 @@ public class BatchXMLAnalysis {
 			}
 		}
 	}
-	
+
 	private void delete(File f) throws IOException {
-		  if (f.isDirectory()) {
-			    for (File c : f.listFiles())
-			      delete(c);
-			  }
-			  if (!f.delete())
-			    throw new FileNotFoundException("Failed to delete file: " + f);
+		if (f.isDirectory()) {
+			for (File c : f.listFiles())
+				delete(c);
+		}
+		if (!f.delete())
+			throw new FileNotFoundException("Failed to delete file: " + f);
 	}
 
 	private static <T> void listToCSV(ArrayList<T> l, String fileName) {
@@ -171,8 +171,8 @@ public class BatchXMLAnalysis {
 					for (int j = 0; j < l.size(); j++) {
 						List<?> col = (List<?>) l.get(j);
 						try {
-						fW.append(String.valueOf(col.get(i)));
-						fW.append(NEW_COLUMN_SEPARATOR);
+							fW.append(String.valueOf(col.get(i)));
+							fW.append(NEW_COLUMN_SEPARATOR);
 						} catch (Exception e) {}
 					}
 					fW.append(NEW_LINE_SEPARATOR);
@@ -196,8 +196,6 @@ public class BatchXMLAnalysis {
 	}
 
 	public static void main(String[] args) { 
-		System.out.println("here");
-		BatchXMLAnalysis analysis = new BatchXMLAnalysis("BorrowingComparisons/BeastXMLs", "BorrowingComparisons/BeastXMLs","BorrowingComparisons");
-		analysis.analyseTopology("BorrowingComparisons/Results");
+		BatchXMLAnalysis analysis = new BatchXMLAnalysis("C:/Users/Stuart/Downloads/BorrowingComparisons/BeastXMLs", "C:/Users/Stuart/Downloads/BorrowingComparisons/BeastXMLs","C:/Users/Stuart/Downloads/BorrowingComparisons");
 	}
 }
